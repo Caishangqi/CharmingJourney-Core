ddd273d3a07315d29486c391384119d5ffdc0cf6
CaIzii <2470159730@qq.com>
2025-01-21 16:42:47 -0600

    SiltBlock: add silt block texture with default block-state

--- src/main/java/org/caizii/charmingjourney/core/common/block/SiltBlock.java
+++ src/main/java/org/caizii/charmingjourney/core/common/block/SiltBlock.java
@@ -1,11 +1,16 @@
 package org.caizii.charmingjourney.core.common.block;
 
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredItem;
+
+import static org.caizii.charmingjourney.core.common.CharmingJourney.BLOCKS;
+import static org.caizii.charmingjourney.core.common.CharmingJourney.ITEMS;
 
 public class SiltBlock extends Block {
+
     public SiltBlock(Properties properties) {
         super(properties);
     }
-
-
 }
