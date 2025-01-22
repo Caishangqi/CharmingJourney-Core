7d3240f32b8a6df928791921a84cd7b47bcc5488
CaIzii <2470159730@qq.com>
2025-01-22 02:41:16 -0600

    SiltBlock: move static Deferred block into register because of eventbus lifecycle

--- src/main/java/org/caizii/charmingjourney/core/common/block/SiltBlock.java
+++ src/main/java/org/caizii/charmingjourney/core/common/block/SiltBlock.java
@@ -1,12 +1,6 @@
 package org.caizii.charmingjourney.core.common.block;
 
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredItem;
-
-import static org.caizii.charmingjourney.core.common.CharmingJourney.BLOCKS;
-import static org.caizii.charmingjourney.core.common.CharmingJourney.ITEMS;
 
 public class SiltBlock extends Block {
 
